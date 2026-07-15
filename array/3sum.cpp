1e28c570e9660b53bcb5b3b9c4a181a378214d43
Hridey Agarwal <67671401+hrideyagarwal@users.noreply.github.com>
2026-07-15 16:38:52 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.cpp
+++ array/3sum.cpp
@@ -13,8 +13,12 @@ if(nums[i]+nums[l]+nums[r]>0) r--;
 else if(0>nums[i]+nums[l]+nums[r])l++;
 else if(0==nums[i]+nums[l]+nums[r]){
     res.push_back({nums[i] , nums[l] , nums[r]});
+    while(l < r && nums[l] == nums[l+1])
+    l++;
+    while(l < r && nums[r] == nums[r-1])
+    r--;
     l++ ; r--; 
-    
+
 }}
         }
         return res;
