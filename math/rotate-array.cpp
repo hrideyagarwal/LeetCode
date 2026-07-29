40e91d3f06ba94b362199e52446a3f52356ad1de
Hridey Agarwal <67671401+hrideyagarwal@users.noreply.github.com>
2026-07-29 17:43:23 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- math/rotate-array.cpp
+++ math/rotate-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
         k=k%n;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
