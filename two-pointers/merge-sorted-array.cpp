164fb40dcad950c518862443087d23614ad1de96
Hridey Agarwal <67671401+hrideyagarwal@users.noreply.github.com>
2026-07-29 22:57:59 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- two-pointers/merge-sorted-array.cpp
+++ two-pointers/merge-sorted-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n==0)return ; 
-        if(m==0) return ;
+       
+        
         int j=0;
-        for(int i=n ; i<=nums1.size()-1 ; i++){
+        for(int i=m ; i<nums1.size() ; i++){
            swap(nums1[i], nums2[j]);
            j++;
         }
+        
         sort(nums1.begin(),nums1.end());
     }
 };
