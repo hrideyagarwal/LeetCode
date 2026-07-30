56c980a229b8c59f5d8884e6be67f6eb5d881813
Hridey Agarwal <67671401+hrideyagarwal@users.noreply.github.com>
2026-07-30 14:48:15 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.cpp
+++ array/longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> st(nums.begin(),nums.end());
         
         int longestseq =0;
-        for(int num:nums){
+        for(auto num:st){
             if(st.find(num-1)==st.end()){
                 int length=1;
                 int curr=num;
